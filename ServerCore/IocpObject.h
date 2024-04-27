29cd828f07bfd58e8be1d28bf2b637fb0b7a669a
wooseop <crazyz2000@naver.com>
2024-04-27 20:06:07 +0900

    refactor:함수 선언 위치 수정

--- ServerCore/IocpObject.h
+++ ServerCore/IocpObject.h
@@ -9,13 +9,13 @@ public:
 	IocpObject();
 	virtual ~IocpObject();
 
+	virtual void dispatch(IocpEvent* iocpEvent, uint32 numOfBytes) abstract;
+
 	inline SOCKET getSocket() const { return _socket; }
 	inline HANDLE getHandle() const { return reinterpret_cast<HANDLE>(_socket); }
 
 	inline const shared_ptr<Service> getService() const { return _service.lock(); }
 	inline void setService(weak_ptr<Service> service) { _service = service; }
-
-	virtual void dispatch(IocpEvent* iocpEvent, uint32 numOfBytes) abstract;
 
 protected:
 	SOCKET _socket;
