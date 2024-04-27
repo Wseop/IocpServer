51d81a8fdb04fa65860d030c5afef6d6aaf31521
wooseop <crazyz2000@naver.com>
2024-04-27 19:55:43 +0900

    feat:PacketHandler

--- ServerCore/PacketHandler.cpp
+++ ServerCore/PacketHandler.cpp
@@ -1,2 +1,28 @@
 #include "pch.h"
 #include "PacketHandler.h"
+
+PacketHandlerFunc gPacketHandler[UINT16_MAX];
+
+void PacketHandler::init()
+{
+	sHeaderSize = sizeof(PacketHeader);
+
+	for (uint32 i = 0; i < UINT16_MAX; i++)
+	{
+		gPacketHandler[i] = handleInvalid;
+	}
+}
+
+void PacketHandler::handlePacket(shared_ptr<Session> session, BYTE* packet)
+{
+	PacketHeader* header = reinterpret_cast<PacketHeader*>(packet);
+	uint16 packetTypeIndex = static_cast<uint16>(header->packetType);
+	BYTE* payload = packet + sHeaderSize;
+	uint32 payloadSize = header->packetSize - sHeaderSize;
+	gPacketHandler[packetTypeIndex](session, payload, payloadSize);
+}
+
+void PacketHandler::handleInvalid(shared_ptr<Session> session, BYTE* payload, uint32 payloadSize)
+{
+	cout << format("[PacketHandler] invalid packet.") << endl;
+}
--- ServerCore/PacketHandler.h
+++ ServerCore/PacketHandler.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "SendBuffer.h"
+
 enum class EPacketType : uint16
 {
 
@@ -8,10 +10,40 @@ enum class EPacketType : uint16
 struct PacketHeader
 {
 	EPacketType packetType;
-	uint16 packetSize;
+	uint32 packetSize;
 };
+
+class Session;
+
+using PacketHandlerFunc = function<void(shared_ptr<Session>, BYTE*, uint32)>;
+extern PacketHandlerFunc gPacketHandler[UINT16_MAX];
 
 class PacketHandler
 {
+public:
+	static void init();
+	static void handlePacket(shared_ptr<Session> session, BYTE* packet);
+
+protected:
+	template<typename T>
+	inline static shared_ptr<SendBuffer> makeSendBuffer(EPacketType packetType, T* payload)
+	{
+		uint32 payloadSize = static_cast<uint32>(payload->ByteSizeLong());
+		uint32 packetSize = sizeof(PacketHeader) + payloadSize;
+		
+		shared_ptr<SendBuffer> sendBuffer = make_shared<SendBuffer>(packetSize);
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(sendBuffer->getBuffer());
+		header->packetType = packetType;
+		header->packetSize = packetSize;
+
+		assert(payload->SerializeToArray(header + 1, payloadSize));
+		return sendBuffer;
+	}
+
+private:
+	static void handleInvalid(shared_ptr<Session> session, BYTE* payload, uint32 payloadSize);
+
+private:
+	static uint32 sHeaderSize;
 };
 
